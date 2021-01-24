9a3957ed97e543dac708faabc9b97fb5d18d2af1
lukestringer <luke.stringer@uqconnect.edu.au>
2021-01-24 18:47:41 +1000

    view stuff looks good

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private static final int BORDER = 1;//pixel size of borders for cells, panes, whatever
 
     public static void main(String[] args) {
         //launch sets up the application then calls start()
@@ -16,69 +17,90 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        int topWidth = 1000;
-        int topHeight = 100;
 
-        int bottomWidth = 1000;
-        int bottomHeight = 100;
 
+
+        Scene scene = setupScene();
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.sizeToScene();//because set resizeable has a bug that adds padding
+        stage.show();
+    }
+
+
+    
+
+
+
+
+
+    //VIEW
+
+    private Scene setupScene() {
+        int gridSize = 600;
+        int cells = 10;
+
+        StackPane gridStack = setupGrid(gridSize, cells);
+
+        HBox top = setupHBox();
+        HBox bottom = setupHBox();
+        VBox right = setupVBox();
+        VBox left = setupVBox();
+
+        BorderPane borderPane = new BorderPane(gridStack, top, right, bottom, left);
+        borderPane.setMinSize(gridSize, gridSize);
+
+        StackPane root = new StackPane(borderPane);
+
+        Scene scene = new Scene(root);
+        scene.setFill(Color.GREY);
+
+        return scene;
+    }
+
+    private VBox setupVBox() {
         int rightHeight = 600;
         int rightWidth = 200;
 
-        int leftHeight = 600;
-        int leftWidth = 200;
+        VBox right = new VBox();
+        Rectangle rectangle = new Rectangle(rightWidth - BORDER, rightHeight - BORDER);
+        StackPane stackPane = new StackPane(rectangle);
+        stackPane.setMinSize(rightWidth, rightHeight);
+        right.getChildren().add(stackPane);
+        return right;
+    }
 
-        int gridSize = 600;
-        int gridCellSize = 60;
-        int border = 10;
+    private HBox setupHBox() {
+        int topWidth = 1000;
+        int topHeight = 100;
 
+        HBox hBox = new HBox();
+        Rectangle topRect = new Rectangle(topWidth - BORDER, topHeight - BORDER);
+        StackPane topStack = new StackPane(topRect);
+        topStack.setMinSize(topWidth, topHeight);
+        hBox.getChildren().add(topStack);
+        return hBox;
+    }
+
+    private StackPane setupGrid(int gridSize, int cells) {
+        int gridCellSize = gridSize/cells;
 
         GridPane gridPane = new GridPane();
         StackPane gridStack = new StackPane(gridPane);
-        for (int j = 0; j < 10; j ++) {
-            for (int i = 0; i < 10; i ++) {
-                Rectangle rectangle = new Rectangle(gridCellSize - border, gridCellSize - border);
-                rectangle.setFill(Color.BLACK);
+        for (int j = 0; j < cells; j ++) {
+            for (int i = 0; i < cells; i ++) {
+                int rectSize = gridCellSize - BORDER;
+                Rectangle rectangle = new Rectangle(rectSize, rectSize);
+                if (i%2==0 && j%2==0 || i%2!=0 && j%2!=0) {
+                    rectangle.setFill(Color.web("#292929"));
+                } else {
+                    rectangle.setFill(Color.web("#3b3b3b"));
+                }
                 StackPane stackPane = new StackPane(rectangle);
                 stackPane.setMinSize(gridCellSize, gridCellSize);
                 gridPane.add(stackPane, i, j);
             }
         }
-
-
-        HBox top = new HBox();
-        Rectangle topRect = new Rectangle(topWidth - border, topHeight - border);
-        StackPane topStack = new StackPane(topRect);
-        topStack.setMinSize(topWidth, topHeight);
-        top.getChildren().add(topStack);
-
-        HBox bottom = new HBox();
-        Rectangle bottomRect = new Rectangle(bottomWidth - border, bottomHeight - border);
-        StackPane bottomStack = new StackPane(bottomRect);
-        bottomStack.setMinSize(bottomWidth, bottomHeight);
-        bottom.getChildren().add(bottomStack);
-
-        VBox right = new VBox();
-        Rectangle rectangle = new Rectangle(rightWidth - border, gridSize - border);
-        StackPane stackPane = new StackPane(rectangle);
-        stackPane.setMinSize(rightWidth, rightHeight);
-        right.getChildren().add(stackPane);
-
-        VBox left = new VBox();
-        Rectangle leftRect = new Rectangle(leftWidth - border, leftHeight - border);
-        StackPane leftStack = new StackPane(leftRect);
-        leftStack.setMinSize(leftWidth, leftHeight);
-        left.getChildren().add(leftStack);
-
-        BorderPane borderPane = new BorderPane(gridStack, top, right, bottom, left);
-        borderPane.setMinWidth(gridSize);
-        borderPane.setMinHeight(gridSize);
-        StackPane root = new StackPane(borderPane);
-        Scene scene = new Scene(root);
-        scene.setFill(Color.GREY);
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.sizeToScene();//because set resizeable has a bug that adds padding
-        stage.show();
+        return gridStack;
     }
 }
