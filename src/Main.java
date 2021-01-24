2c3b8740c40fcf3a7b348e9706589cdcd1306966
lukestringer <luke.stringer@uqconnect.edu.au>
2021-01-24 17:39:19 +1000

    potatoes taste delicious if cooked properly

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 
-import GUI.Contoller;
-import GUI.Model;
-import GUI.View;
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,22 +16,69 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        int gameStageWidth, gameStageHeight;//width and height of game area of stage
-        gameStageHeight = gameStageWidth = 600;//game is a square, but can be changed
-        Model model = new Model();
-        View view = new View(model, gameStageWidth, gameStageHeight);
-        new Contoller(view, model);
+        int topWidth = 1000;
+        int topHeight = 100;
 
-        String title = "Snake";
-        stage.setScene(view.getScene());
-        stage.setResizable(false);//maybe one day: resizeable game window
-        //set up scene further here e.g. min height
-        stage.setTitle(title);
+        int bottomWidth = 1000;
+        int bottomHeight = 100;
 
+        int rightHeight = 600;
+        int rightWidth = 200;
+
+        int leftHeight = 600;
+        int leftWidth = 200;
+
+        int gridSize = 600;
+        int gridCellSize = 60;
+        int border = 10;
+
+
+        GridPane gridPane = new GridPane();
+        StackPane gridStack = new StackPane(gridPane);
+        for (int j = 0; j < 10; j ++) {
+            for (int i = 0; i < 10; i ++) {
+                Rectangle rectangle = new Rectangle(gridCellSize - border, gridCellSize - border);
+                rectangle.setFill(Color.BLACK);
+                StackPane stackPane = new StackPane(rectangle);
+                stackPane.setMinSize(gridCellSize, gridCellSize);
+                gridPane.add(stackPane, i, j);
+            }
+        }
+
+
+        HBox top = new HBox();
+        Rectangle topRect = new Rectangle(topWidth - border, topHeight - border);
+        StackPane topStack = new StackPane(topRect);
+        topStack.setMinSize(topWidth, topHeight);
+        top.getChildren().add(topStack);
+
+        HBox bottom = new HBox();
+        Rectangle bottomRect = new Rectangle(bottomWidth - border, bottomHeight - border);
+        StackPane bottomStack = new StackPane(bottomRect);
+        bottomStack.setMinSize(bottomWidth, bottomHeight);
+        bottom.getChildren().add(bottomStack);
+
+        VBox right = new VBox();
+        Rectangle rectangle = new Rectangle(rightWidth - border, gridSize - border);
+        StackPane stackPane = new StackPane(rectangle);
+        stackPane.setMinSize(rightWidth, rightHeight);
+        right.getChildren().add(stackPane);
+
+        VBox left = new VBox();
+        Rectangle leftRect = new Rectangle(leftWidth - border, leftHeight - border);
+        StackPane leftStack = new StackPane(leftRect);
+        leftStack.setMinSize(leftWidth, leftHeight);
+        left.getChildren().add(leftStack);
+
+        BorderPane borderPane = new BorderPane(gridStack, top, right, bottom, left);
+        borderPane.setMinWidth(gridSize);
+        borderPane.setMinHeight(gridSize);
+        StackPane root = new StackPane(borderPane);
+        Scene scene = new Scene(root);
+        scene.setFill(Color.GREY);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.sizeToScene();//because set resizeable has a bug that adds padding
         stage.show();
-
-
-        //todo speed up after each apple by stopping old timeline and replacing with faster one (should timeline and delay be in model, view, or controller?)
-        //https://stackoverflow.com/questions/19549852/javafx-binding-timelines-duration-to-a-property
     }
 }
